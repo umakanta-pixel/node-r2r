762a82ef1c8f7aae59c6e792d3b6eed97eadc268
ukp-code <124569491+umakanta-pixel@users.noreply.github.com>
2023-12-26 14:37:19 +0530

    added vercel json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => res.send('welcome'))
 app.use('/api/v2', require('./routes/routes'));
 
 module.exports = app;
