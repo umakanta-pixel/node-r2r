1b1e9f57931eec90ba7330c2489ac020f8af7f7c
ukp-code <124569491+umakanta-pixel@users.noreply.github.com>
2024-01-03 09:53:01 +0530

    flight controller updated

--- controllers/flight.controller.js
+++ controllers/flight.controller.js
@@ -33,31 +33,57 @@ class flightController {
                     let inputstring = flight.data[0].itineraries[0].duration;
                     // console.log("inputstring",inputstring);
                     const duration = AmadeusHelper.calculateTime(inputstring.toLowerCase());
+                    let segments = flight.data[0].itineraries[0].segments;
+                    let itineraries = [];
+                    let lastArrivaltime = "";
+                    segments.map((segment) => {
+                        //segmentDuration +=AmadeusHelper.calculateTime(segment.duration.toLowerCase())
+                        //itineraries.totalDuration = totalDuration;
+                        var deperture_date = new Date(segment.departure.at);
+                        var arrival_date = new Date(segment.arrival.at);
+
+                        let stopTime = "";
+                        if (lastArrivaltime != "") {
+                            arrival_date = new Date(lastArrivaltime);
+                            var timeDifference = deperture_date.getTime() - arrival_date.getTime();
+                            console.log("arrival_date", timeDifference);
+
+                            var seconds = Math.floor(timeDifference / 1000);
+                            var minutes = Math.floor(seconds / 60);
+                            stopTime = minutes;
+                            lastArrivaltime = "";
+                        }
+                        lastArrivaltime = segment.arrival.at;
+                        itineraries.push({
+                            'segment_duration': AmadeusHelper.calculateTime(segment.duration.toLowerCase()),
+                            'deperture_date': segment.departure.at,
+                            'arrival_date': segment.arrival.at,
+                            "iataCode": segment.arrival.iataCode,
+                            "segment_stop_time": stopTime
+                        })
+
+                    });
+
+                    // console.log(flight.data[0]);
                     flightArray.push({
                         // 'duration': 10,
                         'onway_duration_hours': duration,
                         "flightid": flight.data[0].id,
                         "type": "flight",
-                        "itineraries": flight.data[0].itineraries[0],
+                        "itineraries": {
+                            'segments': itineraries
+                        },
                         "departure_airport": flight.start_airpot,
                         "arrival_airport": flight.end_airport
                     })
                 }
 
             })
-            //console.log(flightArray);
-            // res.send(flightArray);
             return flightArray;
         } catch (error) {
             const stackLines = error.stack.split('\n');
-            // res.send({
-            //     res_code: 201,
-            //     response: "Flight api error.",
-            //     server_message: error.message,
-            //     line: stackLines[1].trim() ?? ''
-            // })
-            console.log("flight error =",error.message);
-            console.log(" error line =",stackLines[1].trim() ?? '');
+            console.log("flight error =", error.message);
+            console.log(" error line =", stackLines[1].trim() ?? '');
         }
 
 
@@ -72,7 +98,7 @@ class flightController {
 
                 details.map((iata) => {
                     if (iata.relevance > 2.0) {
-                        iataCode.push({ "iata": iata.iataCode, "name": iata.name+' '+iata.subType+' '+iata.address.cityName +' '+iata.address.countryName });
+                        iataCode.push({ "iata": iata.iataCode, "name": iata.name + ' ' + iata.subType + ' ' + iata.address.cityName + ' ' + iata.address.countryName });
                     }
                 });
 
--- controllers/trip.controller.js
+++ controllers/trip.controller.js
@@ -15,95 +15,50 @@ class tripController {
                 finalRoutes = { type: 'car', distance: term['distance'], duration: term['duration'], start_address: term['start_address'], end_address: term['end_address'], overview_polyline: route['overview_polyline']['points'], start_place_id: result['geocoded_waypoints'][0]['place_id'], end_place_id: result['geocoded_waypoints'][1]['place_id'] };
             })
         }
-        // console.log(finalRoutes);
         return finalRoutes;
     }
 
     static async flightRoutes(data) {
         var final = [];
-        // $flight = new FlightController();
-        // $flightRoutes = $flight->airportRouteMapDetails($data);
         let flightRoutes = await flightController.getnearestairportDetails(data);
-        // console.log("flightRoutes",flightRoutes);
-        // return $flightRoutes;
-        // foreach ($flightRoutes as $routes) {
-        //     $tmp = ['total_duration' => 0, 'first_stop' => '', 'last_stop' => '', 'modes' => [], 'overview_polyline' => ''];
-        //     $firstAirport = $routes['flightitineraries'][0]['departure_airport'] . ', ' . $routes['flightitineraries'][0]['departure_location'];
-        //     $lastAirport = $routes['flightitineraries'][0]['arrival_airport'] . ', ' . $routes['flightitineraries'][0]['arrival_location'];
-        //     $arrival = str_replace(" ", "T", $routes['flightitineraries'][0]['departure_at']);
-        //     $departure = str_replace(" ", "T", $routes['flightitineraries'][0]['arrival_at']);
-
-        //     $driveOne = $this->shortestDrivingToAirport(['start_point' => $data['start_point'], 'end_point' => $firstAirport, 'arrival' => $arrival]);
-        //     $driveTwo = $this->shortestDrivingToAirport(['start_point' => $lastAirport, 'end_point' => $data['end_point'], 'departure' => $departure]);
-
-        //     $tmp['total_duration'] += $driveOne['duration']['value'] + $driveTwo['duration']['value'] + $routes['onway_duration_hours'];
-        //     $tmp['first_stop'] = $routes['flightitineraries'][0]['departure_airport'];
-        //     // $tmp['last_stop'] = $routes['flightitineraries'][0]['arrival_airport'];
-        //     $tmp['last_stop'] = $data['end_point'];
-        //     array_push($tmp['modes'], $driveOne, $routes, $driveTwo);
-        //     array_push($final, $tmp);
-        // }
         final = await Promise.all(flightRoutes.map(async (routes) => {
             let tmp = { 'total_duration': 0, 'first_stop': routes['departure_airport'], 'last_stop': routes['arrival_airport'], 'overview_polyline': '' };
-            // let firstAirport = routes['departure_airport'];
-            // let lastAirport = routes['arrival_airport'] ;
             let departure = routes['itineraries']['segments'][0]['departure']['at'].replace(" ", "T");
             let arrival = routes['itineraries']['segments'][(routes['itineraries']['segments'].length) - 1]['arrival']['at'].replace(" ", "T");
-
             let driveOne = await tripController.drivingRoutes({ 'start_point': data['start_point'], 'end_point': routes['departure_airport'], 'departure': departure });
             let driveTwo = await tripController.drivingRoutes({ 'start_point': routes['arrival_airport'], 'end_point': data['end_point'], 'arrival': arrival });
-
-            // console.log(driveOne);
-
             tmp['total_duration'] += driveOne['duration']['value'] + driveTwo['duration']['value'] + routes['onway_duration_hours'];
             tmp.modes = [driveOne, routes, driveTwo];
-            // tmp['modes'].push({driveOne, routes, driveTwo});
-            // final.push(tmp);
             return tmp;
         }));
-        // console.log(final);
         return final;
-
-        // const resultArray = await Promise.all(result.map(async (item) => {
-        //     data = await AmadeusHelper.findFlight(item.start_iata.iata, item.end_iata.iata, departure_date);
-        //     return { "data": data, "start_airpot": item.start_iata.name, "end_airport": item.end_iata.name };
-        // }));
     }
 
 
     async combineRouteList(req, res) {
 
         try {
-            // console.log(req.body);
-            // return 1;
             const errors = validationResult(req);
-            // if there is error then return Error
             if (!errors.isEmpty()) {
                 return res.json({
                     res_code: 201,
                     response: errors.array(),
                 });
             }
-
             var responseData = { searchDetails: req.body };
             responseData['routes'] = [];
-
             const [isTransferAvailable, drivingRoute, flightRoutes] = await Promise.all([transferLib.hasLocation(req.body), tripController.drivingRoutes(req.body), tripController.flightRoutes(req.body)]);
-            // console.log(drivingRoute);
             if (isTransferAvailable) {
                 responseData['routes'].push({ total_duration: drivingRoute['duration']['value'], first_stop: drivingRoute['start_address'], last_stop: drivingRoute['end_address'], modes: drivingRoute, overview_polyline: drivingRoute['overview_polyline'] });
             }
 
-            // console.log(flightRoutes);
-
-            flightRoutes.map((flightRoute)=>{
+            flightRoutes.map((flightRoute) => {
                 responseData['routes'].push(flightRoute);
             })
 
             res.send({
                 res_code: 200,
                 response: "Success",
-                // data: flightRoutes
                 data: responseData
             })
 
