4d0c584c452a9aae9665165fe2f9211bbb7def5b
ukp-code <124569491+umakanta-pixel@users.noreply.github.com>
2024-05-13 16:28:47 +0530

    add utils

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,10 @@ const decryption = require("../middlewares/decryption")
 const encryption = require("../middlewares/encryption")
 
 const tripController = require('../controllers/trip.controller');
-const routeDataValidateSchema = require('../validations/route.validation');
 const ferryController = require('../controllers/ferry.controller');
 const hotelController = require('../controllers/hotel.controller');
 
 
-// router.post('/api/v1/trip-planner/routes', tripController.combineRouteList);
-// router.post('/api/v1/trip-planner/routes',routeDataValidateSchema, tripController.combineRouteList);
 router.post('/trip-planner/routes', encryption, decryption, tripController.combineRouteList);
 router.post('/trip-planner/ferry', ferryController.fetchFromAlgolia);
 router.post('/hotel/get-details',hotelController.getHotelDetails);
